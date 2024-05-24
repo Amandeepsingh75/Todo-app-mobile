5ac1ceadfb243bfca62b1c7323de74dc1babb181
Amandeep75 <Amandeep75318gmail.com>
2024-05-24 11:01:33 +0530

    a

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Todo from './components/Todo.tsx'
-import ApiDataCalling from './components/ApiDataCalling.tsx';
 import { ScrollView , SafeAreaView} from 'react-native';
 
 function App(): React.JSX.Element {
